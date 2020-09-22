0cd85951183729f295d1a75f69d0524ac18d9b8a
David Moraes <investtraderrs@gmail.com>
2020-09-22 17:10:11 -0300

    Exercicio Cliente_Produto

---
+++ ExercicioPedido/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioPedido.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ ExercicioPedido/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+using ExercicioPedido.Entities;
+using ExercicioPedido.Entities.Enums;
+
+namespace ExercicioPedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime dataNasc = DateTime.Parse(Console.ReadLine());
+
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, dataNasc);
+            Order order = new Order(DateTime.Now, status, client);
+
+            for (int i=1; i<=n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string nameP = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(nameP, price);
+
+                Console.Write("Quantity: ");
+                int quant = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quant, price, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
+/*Exercício: Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um
+sumário do pedido conforme exemplo (próxima página). Nota: o instante do pedido deve ser
+o instante do sistema: DateTime.Now e dar a seguinte saído na tela:
+
+Enter cliente data:
+Name: Alex Green
+Email: alex@gmail.com
+Birth date (DD/MM/YYYY): 15/03/1985
+Enter order data:
+Status: Processing
+How many items to this order? 2
+Enter #1 item data:
+Product name: TV
+Product price: 1000.00
+Quantity: 1
+Enter #2 item data:
+Product name: Mouse
+Product price: 40.00
+Quantity: 2
+
+ORDER SUMMARY:
+Order moment: 20/04/2018 11:25:09
+Order status: Processing
+Client: Alex Green (15/03/1985) - alex@gmail.com
+Order items:
+TV, $1000.00, Quantity: 1, Subtotal: $1000.00
+Mouse, $40.00, Quantity: 2, Subtotal: $80.00
+Total price: $1080.00
+*/
